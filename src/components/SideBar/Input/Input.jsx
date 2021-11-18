c60a3646bc14fa672f7ffc19ff896b4b7ebcd2f4
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-11-18 07:47:33 -0500

    Working on Input helper component

--- src/components/SideBar/Input/Input.jsx
+++ src/components/SideBar/Input/Input.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Input = ({ placeholder, type }) => {
     return (
-        <div>
+        <Container>
            <input type={type} placeholder={placeholder} /> 
-        </div>
+            <div />
+        </Container>
     )
 }
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    input {
+        max-width: 350px;
+        min-width: 250px;
+        padding: 0 1rem;
+        margin: 0.5rem 0;
+        height: 40px;
+        border: none;
+        background-color: #f5f5f5;
+        box-shadow: 0px 14px 9px -15px rgba(0,0,0,0.3);
+        border-radius: 1rem;
+
+        transition: all 0.2s ease-in;
+        &:hover {
+            transform: translate(-5px);
+        }
+    }
+`
 
 export default Input
